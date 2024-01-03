27168bb53dddf1be9bbade9b84653b915fce95ca
invertedegg <invertedegg435@gmail.com>
2024-01-03 18:05:28 -0500

    Added globals for cell state representation, added fixed font usage in tkinter

--- ConwayLife.py
+++ ConwayLife.py
@@ -4,9 +4,12 @@ import random as rand
 
 # Globals
 dimensions = 25
+alive = "*"
+dead = " "
 root = tk.Tk()
-boardOutput = tk.Label(root, text="")
+boardOutput = tk.Label(root, text="", font="TkFixedFont")
 boardOutput.pack()
+
 
 # Prints game board to terminal for debugging purposes
 def printTerm(board):
@@ -23,39 +26,39 @@ def findStatus(GoLBoard, x, y):
     if y < dimensions - 1:
         if x < dimensions - 1:
             for offset in range(-1, 2):
-                if GoLBoard[y - 1][x + offset] == "*": count += 1
-                if GoLBoard[y + 1][x + offset] == "*": count += 1
-            if GoLBoard[y][x - 1] == "*": count += 1
-            if GoLBoard[y][x + 1] == "*": count += 1
+                if GoLBoard[y - 1][x + offset] == alive: count += 1
+                if GoLBoard[y + 1][x + offset] == alive: count += 1
+            if GoLBoard[y][x - 1] == alive: count += 1
+            if GoLBoard[y][x + 1] == alive: count += 1
         else :
             for offset in range(-1, 1):
-                if GoLBoard[y - 1][x + offset] == "*": count += 1
-                if GoLBoard[y + 1][x + offset] == "*": count += 1
-            if GoLBoard[y - 1][x - wrap] == "*": count += 1
-            if GoLBoard[y][x - 1] == "*": count += 1
-            if GoLBoard[y][x - wrap] == "*": count += 1
-            if GoLBoard[y + 1][x - wrap] == "*": count += 1
+                if GoLBoard[y - 1][x + offset] == alive: count += 1
+                if GoLBoard[y + 1][x + offset] == alive: count += 1
+            if GoLBoard[y - 1][x - wrap] == alive: count += 1
+            if GoLBoard[y][x - 1] == alive: count += 1
+            if GoLBoard[y][x - wrap] == alive: count += 1
+            if GoLBoard[y + 1][x - wrap] == alive: count += 1
     else:
         if x < dimensions - 1:
             for offset in range(-1, 2):
-                if GoLBoard[y - 1][x + offset] == "*": count += 1
-                if GoLBoard[y - wrap][x + offset] == "*": count += 1
-            if GoLBoard[y][x - 1] == "*": count += 1
-            if GoLBoard[y][x + 1] == "*": count += 1
+                if GoLBoard[y - 1][x + offset] == alive: count += 1
+                if GoLBoard[y - wrap][x + offset] == alive: count += 1
+            if GoLBoard[y][x - 1] == alive: count += 1
+            if GoLBoard[y][x + 1] == alive: count += 1
         else :
             for offset in range(-1, 1):
-                if GoLBoard[y - 1][x + offset] == "*": count += 1
-                if GoLBoard[y - wrap][x + offset] == "*": count += 1
-            if GoLBoard[y - 1][x - wrap] == "*": count += 1
-            if GoLBoard[y][x - 1] == "*": count += 1
-            if GoLBoard[y][x - wrap] == "*": count += 1
-            if GoLBoard[y - wrap][x - wrap] == "*": count += 1
+                if GoLBoard[y - 1][x + offset] == alive: count += 1
+                if GoLBoard[y - wrap][x + offset] == alive: count += 1
+            if GoLBoard[y - 1][x - wrap] == alive: count += 1
+            if GoLBoard[y][x - 1] == alive: count += 1
+            if GoLBoard[y][x - wrap] == alive: count += 1
+            if GoLBoard[y - wrap][x - wrap] == alive: count += 1
 
     if count == 3:
         # Survived OR Reproduction
         return 1
     elif count == 2:
-        if GoLBoard[y][x] == "*":
+        if GoLBoard[y][x] == alive:
             # Survived
             return 1
         else:
@@ -72,15 +75,16 @@ def nextGen(board):
     nextBoard = []
     for y in range(0, dimensions):
         nextRow = []
-        for x in range(0, dimensions):
-            if findStatus(board, x, y) == 1: nextRow.append("*")
-            else: nextRow.append(" ")
+        for x in range(0, dimensions * 3):
+            if findStatus(board, x, y) == 1: nextRow.append(alive)
+            else: nextRow.append(dead)
         nextBoard.append(nextRow)
 
     for row in nextBoard:
         for idx in row:
             boardStr += idx
         boardStr += "\n"
+    boardStr.format()
     boardOutput.configure(text=boardStr)
     # Set board global to the next generation
     root.after(1000, nextGen, nextBoard)
@@ -90,9 +94,9 @@ def randPopulate():
     board = []
     for y in range(0, dimensions):
         nextRow = []
-        for x in range(0, dimensions):
-            if rand.randrange(1, 6) == 3: nextRow.append("*")
-            else: nextRow.append(" ")
+        for x in range(0, dimensions * 3):
+            if rand.randrange(1, 11) == 1: nextRow.append(alive)
+            else: nextRow.append(dead)
         board.append(nextRow)
     return board
 
