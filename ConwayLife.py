9ffa343c6a5279724bedabb0e80cec8ce97214e5
invertedegg <invertedegg435@gmail.com>
2024-01-03 16:31:51 -0500

    Added a findStatus function to determine if cells live or die

--- ConwayLife.py
+++ ConwayLife.py
@@ -3,7 +3,9 @@ from tkinter import ttk
 import random as rand
 
 # Globals
+# GOL_BOARD indexing goes [y-axis][x-axis]
 GOL_BOARD =[]
+dimensions = 5
 root = tk.Tk()
 boardOutput = tk.Label(root, text="")
 boardOutput.pack()
@@ -15,17 +17,68 @@ def printTerm(board):
             print(idx, end="")
         print()
 
+def findStatus(x, y):
+    print("caught:", str(y), str(x))
+    wrap = dimensions - 1
+    count = 0
+
+    # Find life status of top/bottom row neighbors
+    if y < dimensions - 1:
+        if x < dimensions - 1:
+            for offset in range(-1, 2):
+                if GOL_BOARD[y - 1][x + offset] == "1": count += 1
+                if GOL_BOARD[y + 1][x + offset] == "1": count += 1
+            if GOL_BOARD[y][x - 1] == "1": count += 1
+            if GOL_BOARD[y][x + 1] == "1": count += 1
+        else :
+            for offset in range(-1, 1):
+                if GOL_BOARD[y - 1][x + offset] == "1": count += 1
+                if GOL_BOARD[y + 1][x + offset] == "1": count += 1
+            if GOL_BOARD[y - 1][x - wrap] == "1": count += 1
+            if GOL_BOARD[y][x - 1] == "1": count += 1
+            if GOL_BOARD[y][x - wrap] == "1": count += 1
+            if GOL_BOARD[y + 1][x - wrap] == "1": count += 1
+    else:
+        if x < dimensions - 1:
+            for offset in range(-1, 2):
+                if GOL_BOARD[y - 1][x + offset] == "1": count += 1
+                if GOL_BOARD[y - wrap][x + offset] == "1": count += 1
+            if GOL_BOARD[y][x - 1] == "1": count += 1
+            if GOL_BOARD[y][x + 1] == "1": count += 1
+        else :
+            for offset in range(-1, 1):
+                if GOL_BOARD[y - 1][x + offset] == "1": count += 1
+                if GOL_BOARD[y - wrap][x + offset] == "1": count += 1
+            if GOL_BOARD[y - 1][x - wrap] == "1": count += 1
+            if GOL_BOARD[y][x - 1] == "1": count += 1
+            if GOL_BOARD[y][x - wrap] == "1": count += 1
+            if GOL_BOARD[y - wrap][x - wrap] == "1": count += 1
+
+    if count == 3:
+        print("Survived OR Reproduction")
+    elif count == 2:
+        if GOL_BOARD[y][x] == "1":
+            print("Survived")
+        else:
+            print("Dead, not enough for Reproduction")
+    else:
+        print("Dead")
+    print()
+
+
 def nextGen():
     boardStr = ""
     print("updated")
 
     temp = []
-    for x in range(0, 9):
+    for y in range(0, dimensions):
         nextRow = []
-        for y in range(0, 9):
-            if rand.randrange(1, 6) == 3: nextRow.append(str(1))
-            else: nextRow.append(str(0))
-        temp.append(nextRow)
+        for x in range(0, dimensions):
+            print("Find status on point:", str(y), str(x))
+            findStatus(x, y)
+            # if rand.randrange(1, 6) == 3: nextRow.append(str(1))
+            # else: nextRow.append(str(0))
+        # temp.append(nextRow)
 
     for row in temp:
         for idx in row:
@@ -36,23 +89,25 @@ def nextGen():
 
 # Randomly populates the game board for the initial state
 def randPopulate():
-    for x in range(0, 9):
+    for y in range(0, dimensions):
         nextRow = []
-        for y in range(0, 9):
+        for x in range(0, dimensions):
             if rand.randrange(1, 6) == 3: nextRow.append(str(1))
             else: nextRow.append(str(0))
         GOL_BOARD.append(nextRow)
 
+
 def main():
     # Window initializations
-    root.title("Conway's Game of Life")
-    root.geometry("400x400+200+100")
+    # root.title("Conway's Game of Life")
+    # root.geometry("400x400+200+100")
 
-    printTerm(GOL_BOARD)
     randPopulate()
-    root.after(1000, nextGen)
+    printTerm(GOL_BOARD)
+    nextGen()
+    # root.after(1000, nextGen)
 
-    root.mainloop()
+    # root.mainloop()
 
 if __name__ == "__main__":
     main()
